881496cab47b62f635dcce66d137d81edfa3ae47
hennadii-kolesnyk <hennadii.kolesnyk@gmail.com>
2020-12-28 13:32:35 +0200

    Update portAudio.py
    master - add description

--- portAudio.py
+++ portAudio.py
@@ -14,34 +14,53 @@ import utility
 
 st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
+# Ініціалізуємо екземпляр фасада PyAudio
 p = pyaudio.PyAudio()
+# Дістаємо інформацію про доступну периферію
 info = p.get_host_api_info_by_index(0);
+# Отримуємо кількість достувних пристроїв
 numdevices = info.get('deviceCount')
+# Ініціалізуємо цикл обходу по всіх пристроях
 for i in range(0, numdevices):
+        # Отримуємо обєкт пристрою, який містить інформацію про пристрій
         dev = p.get_device_info_by_host_api_device_index(0, i);
+        # Якщо пристрій має менше 1 каналу, він нас не цікавить, інакше виводимо інформацію
         if (dev.get('maxInputChannels')) > 0:
+            # Фунція виводу тексту в термінал, обєднуємо данні про пристрій для його подальшого вибору
             print (
                 "Input Device id "+ str(i)+ " - "+ dev.get('name')+ 
                 ", Channels count: "+ str(dev.get('maxInputChannels')) +
                 ", Default sample rate: " + str(dev.get('defaultSampleRate'))
             )
 
+# Перевіряємо на помилку
 try:
+    #Ініціалізуємо ввід користувача в конольному інтерфейсі, та присвоюємо ідентифікатор обраного пристрою
     input = raw_input
 except NameError:
+    # Якщо помилка,то нічого не робимо
     pass
 
+# Обираємо пристрій, той що ввів користувач
 device = p.get_device_info_by_host_api_device_index(0, int(input("Chose device: ")));
 
+# По скільки байт будемо считувати з буфера
 CHUNK = 1024;
+# Формат данних, те саме що int24, де 24 розрядність
 FORMAT = pyaudio.paInt24;
+# Дістаємо з пристрою кількість всіх доступних каналів
 CHANNELS = int(device.get('maxInputChannels'));
+# Дістаємо частоту пристрою, за замовченням, можна встановити вручну, як закоментовано нижче.
 RATE = int(device.get('defaultSampleRate'));
 #RATE = 192000;
+# Індекс пристрою, для того щоб PyAudio розуміло з чим працювати
 INPUT_DEVICE_INDEX = int(device.get('index'));
+# Час зчитування у секундах
 RECORD_SECONDS = 3
+# Ім'я файлу(можна задати будь яке)
 WAVE_OUTPUT_FILENAME = st+".wav"
 
+# За добопогою визначених констант та змінних, створюємо потік, який звертаєтся до пристрою та зчитує данні його буферу обміну
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
@@ -49,20 +68,31 @@ stream = p.open(format=FORMAT,
                 input=True,
                 frames_per_buffer=CHUNK)
 
+# Вивід для розуміння, що процес зчитування почався.
 print("start recording")
 
+# Ініціалізуємо структуру, де будема зберігати данні буферу обраного пристрою.
 frames = []
 
+# В залежності від частоти, кроку та кількості секунд, ініціалізуємо цикл зчитування, який буде тривати 3 секунди
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    # Присвоюєм зчитаний фрейм з обраним кроком у проміжну змінну
     data = stream.read(CHUNK)
+    # Додаємо фрейм до структури фреймів.
     frames.append(data)
 
+# Виводимо кількість фреймів
 print(len(frames));
+# Вивід для розуміння, що процес зчитування закінчився
 print("stop recording")
 
+# Зупиняєо процес зчитування
 stream.stop_stream()
+# Звільняємо память потоку
 stream.close()
+# Звільняємо память бібліотеки
 p.terminate()
+
 
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 wf.setnchannels(CHANNELS)
